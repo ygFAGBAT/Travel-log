aa63bf92804945d3e7fb9dea1aa5e288eac9f75d
ygFAGBAT <yg6952@utexas.edu>
2020-12-14 23:57:43 +0800

    set up mangoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const middlewares = require('./middlewares');
+
+mongoose.connect('mongodb://localhost/test',{
+    useNewUrlParser:true,
+});
 
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World!',
+    });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
